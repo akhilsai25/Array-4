48aa406f0c244d9b2be7826ad878321342e47cd2
akhilsai25 <38877327+akhilsai25@users.noreply.github.com>
2025-09-24 17:22:15 -0500

    Create MaxSubArray.java

---
+++ MaxSubArray.java
@@ -0,0 +1,11 @@
+// This solution uses a concept of having running sum  updated based on max value encountered in either runningsum+current element or just current element.
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0], currSum = nums[0];
+        for(int i=1;i<nums.length;i++) {
+            currSum = Math.max(currSum+nums[i], nums[i]);
+            maxSum=Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+}
